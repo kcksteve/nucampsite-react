963bbe1dbfb6134d9896fc2b44614d44c3fbcf74
kcksteve <kcksteve@gmail.com>
2022-06-14 23:17:16 -0400

    Update App.js, Footer.js, and 18 more files... Added router elements (breadcrumbs and routing)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import {Routes, Route} from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
 import './App.css';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -7,7 +11,15 @@ function App() {
     return (
         <div className='App'>
             <Header/>
-            <CampsitesDirectoryPage/>
+            <Routes>
+                <Route path='/' element={<HomePage/>}/>
+                <Route path='contact' element={<ContactPage/>}/>
+                <Route path='directory' element={<CampsitesDirectoryPage/>}/>
+                <Route
+                    path='directory/:campsiteId'
+                    element={<CampsiteDetailPage />}
+                />
+            </Routes>
             <Footer/>
         </div>
     );
