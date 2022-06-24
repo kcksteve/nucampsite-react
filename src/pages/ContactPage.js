f5a498cc5ac9b1a27f342f401a6d66fe08b43958
kcksteve <kcksteve@gmail.com>
2022-06-23 21:22:48 -0400

    Update ContactForm.js, ContactPage.js, and validateContactForm.js added contact form with valiadation

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,5 +1,6 @@
 import { Container, Col, Row } from "reactstrap";
 import SubHeader from "../components/SubHeader";
+import ContactForm from "../features/comments/ContactForm";
 
 const ContactPage = () => {
     return (
@@ -40,7 +41,7 @@ const ContactPage = () => {
                     <hr/>
                 </Col>
                 <Col md='12'>
-                    TBD: ContactForm
+                    <ContactForm/>
                 </Col>
             </Row>
         </Container>
